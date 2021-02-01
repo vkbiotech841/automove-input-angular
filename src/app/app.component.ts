20c15f135204e9fecaca7018d5bbcf2b98eb9161
VIKRAM KUMAR <vkbiotech841@gmail.com>
2021-02-01 13:05:50 +0530

    finished adding automove input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'automove-inputbox-angular';
+
+  otp: any = "";
+  showOtpComponent = true;
+
+  @ViewChild('ngOtpInput', { static: false }) ngOtpInput: any;
+
+
+
+
+  config = {
+    allowNumbersOnly: false,
+    length: 5,
+    isPasswordInput: false,
+    disableAutoFocus: false,
+    placeholder: '',
+    inputStyles: {
+      'width': '50px',
+      'height': '50px',
+      'border': '0px',
+      'border-bottom': '2px solid black',
+      'border-radius': '0px'
+    }
+  };
+
+  onOtpChange(otp: any) {
+    this.otp = otp;
+  }
+
+  setVal(val: any) {
+    this.ngOtpInput.setValue(val);
+  }
+
+  toggleDisable() {
+    if (this.ngOtpInput.otpForm) {
+      if (this.ngOtpInput.otpForm.disabled) {
+        this.ngOtpInput.otpForm.enable();
+      } else {
+        this.ngOtpInput.otpForm.disable();
+      }
+    }
+  }
+
+  onConfigChange() {
+    this.showOtpComponent = false;
+    this.otp = null;
+    setTimeout(() => {
+      this.showOtpComponent = true;
+    }, 0);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgOtpInputModule } from 'ng-otp-input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOtpInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
